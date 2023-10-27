38deda8f910d0df36e24af9542ad5d4cb9c29c3c
Uriel Gonzalez <gonzalezlugou@gmail.com>
2023-10-26 23:31:09 -0600

    Primer Commit, Proyecto base

---
+++ BackDestiCode/Data/Context/ApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackDestiCode.Data.Context
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) :base(options)
+        {
+            
+        }
+    }
+}
---
+++ BackDestiCode/Program.cs
@@ -0,0 +1,40 @@
+using BackDestiCode.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Configuracion de la cadena de conexion a Base de datos con SQL
+builder.Services.AddDbContext<ApiDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StrConnection"));
+});
+
+// Configuracion de la inyeccion de dependencias de las Interfaces y Repositorios de la app  
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//Configuracion del uso de cors de la Api
+app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().WithOrigins("*"));
+
+app.MapControllers();
+
+app.Run();
